2a48fb48d9f667b5b3877d5da9aab2dc57654a15
Allen <avalmercafe02@gmail.com>
2022-12-05 22:14:34 -0500

    commit tree a4200416338577bf89fa2d0ebdccf747d54a9022

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,11 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   attr_accessor :name
   attr_reader :songs
+  
 
   @@artists = []
 
@@ -11,26 +14,18 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def add_song(song)
     @songs << song
     song.artist = self
   end
+  
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   attr_accessor :name
   attr_reader :artist
+  
 
   @@songs = []
 
@@ -10,20 +13,10 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
